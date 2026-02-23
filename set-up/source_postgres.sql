f2e8156dd04ef218e843f20e6b52513c2858194e
Deepak Mishra <deepak.m.careers@gmail.com>
2026-02-23 19:39:04 +0530

    Update source_postgres.sql

--- set-up/source_postgres.sql
+++ set-up/source_postgres.sql
@@ -1,10 +1,12 @@
--- 1. Create the schema for raw data
+-- 1. CREATE DATABASE {{ env_var('POSTGRES_DB') }};
+
+-- 2. Create the schema for raw data
 CREATE TABLE raw_customers (id INT PRIMARY KEY, first_name VARCHAR(255), last_name VARCHAR(255));
 CREATE TABLE raw_orders (id INT PRIMARY KEY, user_id INT, order_date DATE, status VARCHAR(50));
 CREATE TABLE raw_payments (id INT PRIMARY KEY, order_id INT, payment_method VARCHAR(100), amount INT);
 
--- 2. Create the Logical Replication Slot for this specific database
+-- 3. Create the Logical Replication Slot for this specific database
 SELECT * FROM pg_create_logical_replication_slot('Name_of_the_Slot', 'pgoutput');
 
--- 3. Create the Publication for your tables
+-- 4. Create the Publication for your tables
 CREATE PUBLICATION name_of_the_publication FOR ALL TABLES;
