65e78a8506460d1363498652cf48048bd95898c9
Deepak Mishra <deepak.m.careers@gmail.com>
2026-02-24 12:06:56 +0530

    Update target_snowflake.sql

--- set-up/target_snowflake.sql
+++ set-up/target_snowflake.sql
@@ -1,5 +1,5 @@
 -- 1. Create a dedicated Database
-CREATE DATABASE {{ env_var('SNOWFLAKE_DATABASE') }};
+CREATE DATABASE {{ env_var('SNOW_DB') }};
 
 -- 2. Create a Warehouse (Compute) for dbt to use later
-CREATE WAREHOUSE {{ env_var('SNOWFLAKE_WAREHOUSE') }} WITH WAREHOUSE_SIZE = 'XSMALL' AUTO_SUSPEND = 60;
+CREATE WAREHOUSE {{ env_var('SNOW_WH') }} WITH WAREHOUSE_SIZE = 'XSMALL' AUTO_SUSPEND = 60;
