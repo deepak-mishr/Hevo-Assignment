8a32a2932455427171a5aba7a25dc8b56aab5536
Deepak Mishra <deepak.m.careers@gmail.com>
2026-02-26 17:32:37 +0530

    Delete set-up/target_snowflake.sql

--- set-up/target_snowflake.sql
+++
@@ -1,5 +0,0 @@
--- 1. Create a dedicated Database
-CREATE DATABASE your_sf_database_name;
-
--- 2. Create a Warehouse (Compute) for dbt to use later
-CREATE WAREHOUSE your_sf_warehouse_name WITH WAREHOUSE_SIZE = 'XSMALL' AUTO_SUSPEND = 60;
